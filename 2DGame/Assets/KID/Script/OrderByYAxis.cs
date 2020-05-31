88c46436721713033464775b6c420032c0c76be3
KID <50666207+KID421@users.noreply.github.com>
2020-05-31 23:16:41 +0800

    利用 Y 軸調整順序

---
+++ 2DGame/Assets/KID/Script/OrderByYAxis.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    public class OrderByYAxis : MonoBehaviour
+    {
+        [Header("位移")]
+        public float offset;
+
+        private SpriteRenderer spr;
+
+        private void Awake()
+        {
+            spr = GetComponent<SpriteRenderer>();
+        }
+
+        private void Update()
+        {
+            spr.sortingOrder = Mathf.RoundToInt((transform.position.y + offset) * -100);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position + transform.up * offset, 0.1f);
+        }
+    }
+}
---
+++ 2DGame/Assets/KID/Script/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    public class Player : MonoBehaviour
+    {
+        [Header("移動速度"), Range(0, 100)]
+        public float speed = 10;
+
+        private Animator ani;
+
+        private void Start()
+        {
+            ani = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            Move();
+        }
+
+        /// <summary>
+        /// 移動
+        /// </summary>
+        private void Move()
+        {
+            float v = Input.GetAxis("Vertical");
+            float h = Input.GetAxis("Horizontal");
+
+            transform.Translate(h * Time.deltaTime * speed, v * Time.deltaTime * speed, 0);
+        }
+    }
+}
